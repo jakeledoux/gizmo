df9029c05184e12a2dd53a52d65d969058f66ee0
Jake Ledoux <contactjakeledoux@gmail.com>
2025-05-10 16:32:31 -0400

    beginning of event/entity structure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,117 @@
 use bevy::{image::ImageSamplerDescriptor, prelude::*};
 
+enum LifeStatus {
+    Alive,
+    Dead,
+}
+
 #[derive(Component)]
-struct Dialogue {
-    name: String,
-    dialogue: String,
-    responses: Vec<String>,
+struct Player;
+
+#[derive(Component)]
+struct Npc;
+
+#[derive(Component)]
+struct RpgEntity {
+    name: &'static str,
+    damage: f32,
+}
+
+impl RpgEntity {
+    pub fn new(name: &'static str) -> Self {
+        Self { name, damage: 0.0 }
+    }
+
+    pub fn apply_damage(&mut self, damage: f32) -> LifeStatus {
+        self.damage += damage;
+
+        if self.damage < self.max_health() {
+            return LifeStatus::Alive;
+        }
+        self.damage = self.max_health();
+        LifeStatus::Dead
+    }
+
+    pub fn max_health(&self) -> f32 {
+        20.0
+    }
+
+    pub fn health(&self) -> f32 {
+        self.max_health() - self.damage
+    }
+}
+
+impl std::fmt::Display for RpgEntity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+#[derive(Event)]
+struct AttackEvent {
+    attacker: Entity,
+    victim: Entity,
+}
+
+impl AttackEvent {
+    fn handler(
+        query: Query<(Entity, &RpgEntity)>,
+        mut attack_events: EventReader<AttackEvent>,
+        mut damage_event: EventWriter<DamageEvent>,
+    ) {
+        for &AttackEvent { attacker, victim } in attack_events.read() {
+            let [attacker, victim] = query.get_many([attacker, victim]).unwrap();
+            info!("{} attacked {}!", attacker.1, victim.1);
+
+            damage_event.write(DamageEvent {
+                victim: victim.0,
+                damage: 10.0,
+            });
+        }
+    }
+}
+
+#[derive(Event)]
+struct DamageEvent {
+    victim: Entity,
+    damage: f32,
+}
+
+impl DamageEvent {
+    fn handler(
+        mut query: Query<(Entity, &mut RpgEntity)>,
+        mut damage_events: EventReader<DamageEvent>,
+        mut death_event: EventWriter<DeathEvent>,
+    ) {
+        for &DamageEvent { victim, damage } in damage_events.read() {
+            let mut victim = query.get_mut(victim).unwrap();
+            match victim.1.apply_damage(damage) {
+                LifeStatus::Alive => {
+                    info!("{} health is now: {}", *victim.1, victim.1.health())
+                }
+                LifeStatus::Dead => {
+                    death_event.write(DeathEvent(victim.0));
+                }
+            };
+        }
+    }
+}
+
+#[derive(Event)]
+struct DeathEvent(Entity);
+
+impl DeathEvent {
+    fn handler(
+        query: Query<&RpgEntity>,
+        mut commands: Commands,
+        mut death_events: EventReader<DeathEvent>,
+    ) {
+        for &DeathEvent(victim_id) in death_events.read() {
+            let victim = query.get(victim_id).unwrap();
+            info!("{} has died", victim);
+            commands.entity(victim_id).despawn();
+        }
+    }
 }
 
 fn main() {
@@ -12,134 +119,64 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin {
             default_sampler: ImageSamplerDescriptor::nearest(),
         }))
-        .add_systems(Startup, (setup, setup_ui))
-        .add_systems(Update, exit_on_esc)
+        .add_event::<AttackEvent>()
+        .add_event::<DamageEvent>()
+        .add_event::<DeathEvent>()
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (
+                debug_attack_system,
+                debug_show_all_entities_system,
+                exit_on_esc,
+            ),
+        )
+        // event handlers
+        .add_systems(
+            Update,
+            (
+                AttackEvent::handler,
+                DamageEvent::handler,
+                DeathEvent::handler,
+            ),
+        )
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
+
+    commands.spawn((Player, RpgEntity::new("Jake")));
+    commands.spawn((Npc, RpgEntity::new("Boba Fett")));
 }
 
-fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("fonts/VT323-Regular.ttf");
-    let image = asset_server.load("nate.png");
+fn debug_attack_system(
+    player_query: Query<Entity, With<Player>>,
+    npc_query: Query<Entity, With<Npc>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut attack_event: EventWriter<AttackEvent>,
+) {
+    let (Ok(player), Ok(npc)) = (player_query.single(), npc_query.single()) else {
+        return;
+    };
 
-    let name = "Nate";
-    let dialogue = "This is what Nate is saying. He's saying lots of things but not realy meaning any of it. This is all just to take up space while I work on programming the layouts.";
-    let responses = vec!["yes, yes!", "a partridge in a pear tree?", "goodbye!"];
-
-    let padding = Val::Px(10.0);
-
-    commands
-        .spawn((
-            Name::new("Conversation Window"),
-            Node {
-                display: Display::Grid,
-                width: Val::Px(800.0),
-                height: Val::Px(400.0),
-                grid_template_columns: vec![GridTrack::auto()],
-                row_gap: padding,
-                column_gap: padding,
-                padding: UiRect::all(padding),
-                ..default()
-            },
-            BackgroundColor(Color::srgb(0.0, 0.3, 0.7)),
-        ))
-        .with_children(|builder| {
-            builder.spawn((
-                Name::new("Header"),
-                Text::new("Conversation"),
-                TextFont {
-                    font: font.clone(),
-                    ..default()
-                },
-                TextColor(Color::BLACK),
-                TextLayout {
-                    justify: JustifyText::Center,
-                    ..default()
-                },
-            ));
-            builder
-                .spawn((
-                    Name::new("NPC Row"),
-                    Node {
-                        display: Display::Grid,
-                        grid_template_columns: vec![
-                            GridTrack::percent(50.0),
-                            GridTrack::min_content(),
-                        ],
-                        column_gap: padding,
-                        ..default()
-                    },
-                ))
-                .with_children(|builder| {
-                    builder
-                        .spawn((
-                            Name::new("dialogue box"),
-                            Node {
-                                display: Display::Grid,
-                                grid_template_rows: vec![
-                                    GridTrack::min_content(),
-                                    GridTrack::auto(),
-                                ],
-                                ..default()
-                            },
-                            BackgroundColor(Color::hsl(0.0, 0.0, 0.1)),
-                        ))
-                        .with_children(|builder| {
-                            builder.spawn((
-                                Text::new(name),
-                                TextFont {
-                                    font: font.clone(),
-                                    ..default()
-                                },
-                                TextLayout {
-                                    justify: JustifyText::Center,
-                                    ..default()
-                                },
-                                TextColor(Color::WHITE),
-                            ));
-                            builder.spawn((
-                                Text::new(dialogue),
-                                TextFont {
-                                    font: font.clone(),
-                                    ..default()
-                                },
-                                TextColor(Color::WHITE),
-                            ));
-                        });
-                    builder
-                        .spawn((Name::new("Image container"), Node { ..default() }))
-                        .with_children(|builder| {
-                            builder.spawn(ImageNode { image, ..default() });
-                        });
-                });
-            builder
-                .spawn((
-                    Name::new("Responses"),
-                    Node {
-                        display: Display::Grid,
-                        width: Val::Percent(100.0),
-                        height: Val::Auto,
-                        grid_template_columns: vec![GridTrack::auto()],
-                        ..default()
-                    },
-                    BackgroundColor(Color::hsl(0.0, 0.0, 0.1)),
-                ))
-                .with_children(|builder| {
-                    responses.into_iter().for_each(|response| {
-                        builder.spawn((
-                            Text::new(response),
-                            TextFont {
-                                font: font.clone(),
-                                ..default()
-                            },
-                            TextColor(Color::WHITE),
-                        ));
-                    })
-                });
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        attack_event.write(AttackEvent {
+            attacker: player,
+            victim: npc,
         });
+    }
+}
+
+fn debug_show_all_entities_system(
+    query: Query<&RpgEntity>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Backspace) {
+        for rpg_entity in query.iter() {
+            info!("{rpg_entity} still exists");
+        }
+    }
 }
 
 fn exit_on_esc(keyboard_input: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
